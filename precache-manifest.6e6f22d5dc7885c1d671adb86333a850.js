2ef5f9b27f1d27c975c347400701965ae4e32b7e
adica <adic@playbuzz.com>
2018-10-05 13:07:35 +0300

    Updates

---
+++ precache-manifest.6e6f22d5dc7885c1d671adb86333a850.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9061ff665ca8ca5bb17c",
+    "url": "/react-todo-list/static/js/runtime~main.9061ff66.js"
+  },
+  {
+    "revision": "7b3d9cb01cf1c4a39a0f",
+    "url": "/react-todo-list/static/js/main.7b3d9cb0.chunk.js"
+  },
+  {
+    "revision": "0f373cbbc7fc7de6ddff",
+    "url": "/react-todo-list/static/js/1.0f373cbb.chunk.js"
+  },
+  {
+    "revision": "7b3d9cb01cf1c4a39a0f",
+    "url": "/react-todo-list/static/css/main.05c57150.chunk.css"
+  },
+  {
+    "revision": "9841a778a8331981652c25b3ee0e16ca",
+    "url": "/react-todo-list/index.html"
+  }
+];
